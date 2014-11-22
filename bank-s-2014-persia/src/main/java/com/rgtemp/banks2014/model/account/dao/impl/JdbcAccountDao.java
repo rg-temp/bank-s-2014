364eeca10378d9d3006c8e8697788fd732fe6000
rg-temp <rg-temp@yandex.com>
2014-11-22 19:46:47 +0100

    Fix autowiring for Datasource.

--- bank-s-2014-persia/src/main/java/com/rgtemp/banks2014/model/account/dao/impl/JdbcAccountDao.java
+++ bank-s-2014-persia/src/main/java/com/rgtemp/banks2014/model/account/dao/impl/JdbcAccountDao.java
@@ -1,7 +1,10 @@
 package com.rgtemp.banks2014.model.account.dao.impl;
 
+import javax.sql.DataSource;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.rgtemp.banks2014.model.account.dao.AccountDao;
 
 @Repository
@@ -10,6 +13,12 @@ public class JdbcAccountDao extends JdbcDaoSupport implements AccountDao {
  * Possible improvement:
  * Configure bean annotation to have id="accountDAO" instead of id="jdbcAccountDAO" 
  */
+	
+	@Autowired 
+	public JdbcAccountDao(DataSource dataSource) {
+	    super();
+	    setDataSource(dataSource);
+	}
 	
 	@Override
 	public void delete(Integer id) {
